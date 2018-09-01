0acac149ff85ff6dc120cae8f37b8896cc43a44e
j1nma <j1nma@users.noreply.github.com>
2018-09-01 18:53:31 -0300

    First commit.

---
+++ server/src/main/java/tp4/server/Server.java
@@ -0,0 +1,12 @@
+package tp4.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("ex1 Server Starting ...");
+    }
+}
